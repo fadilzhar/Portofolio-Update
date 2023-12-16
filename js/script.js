d2594ef87d191a35ba1e2065dad5a715986f190a
fadilzhar <fadilathazharif@gmail.com>
2023-12-16 17:46:26 +0700

    First Branch

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text',{
+    strings:['Front-End Web Designer', 'Active English Speaker','Video Editor'],
+    typeSpeed:35,
+    backSpeed:35,
+    backDelay:100,
+    loop:true,
+})
